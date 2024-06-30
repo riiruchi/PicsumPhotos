c131e6010b965920ef9239cf9ad61803e2588aa1
Ruchira Sinha <ruchirasinha0@gmail.com>
2024-06-30 19:12:25 +0530

    [Implementation]Updating the Network class, Removed Alamofire instead using UrlSession

--- PicsumPhotos/Managers/NetworkManager.swift
+++ PicsumPhotos/Managers/NetworkManager.swift
@@ -6,21 +6,33 @@
 //
 
 import Foundation
-import Alamofire
 
 class NetworkManager {
+    
     static let shared = NetworkManager()
+    
+    private let baseURL = "https://picsum.photos/v2/list"
+    
     private init() {}
-
+    
     func fetchPhotos(page: Int, limit: Int, completion: @escaping (Result<[Photo], Error>) -> Void) {
-        let urlString = "https://picsum.photos/v2/list?page=\(page)&limit=\(limit)"
-        AF.request(urlString).responseDecodable(of: [Photo].self) { response in
-            switch response.result {
-            case .success(let photos):
-                completion(.success(photos))
-            case .failure(let error):
+        guard let url = URL(string: "\(baseURL)?page=\(page)&limit=\(limit)") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
                 completion(.failure(error))
+                return
             }
-        }
+            
+            guard let data = data else { return }
+            
+            do {
+                let photos = try JSONDecoder().decode([Photo].self, from: data)
+                completion(.success(photos))
+            } catch let jsonError {
+                completion(.failure(jsonError))
+            }
+        }.resume()
     }
 }
+
