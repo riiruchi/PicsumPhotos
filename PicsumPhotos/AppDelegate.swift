7ed43e888aab824d1e3bf39db97522449d07041e
Ruchira Sinha <ruchirasinha0@gmail.com>
2024-06-30 13:55:55 +0530

    [Implementation]Minimum deployment target of iOS 12.0

---
+++ PicsumPhotos/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  PicsumPhotos
+//
+//  Created by Ruchira  on 30/06/24.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    // For iOS 13 and later
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+            return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // This function is required by the protocol but does not need any implementation for basic usage.
+    }
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if #available(iOS 13.0, *) {
+            // The window will be configured in the SceneDelegate for iOS 13 and later
+        } else {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            let viewController = ViewController()
+            window?.rootViewController = viewController
+            window?.makeKeyAndVisible()
+        }
+        return true
+    }
+}
+
